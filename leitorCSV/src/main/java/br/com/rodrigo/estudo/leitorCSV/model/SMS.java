f157cbd518f37274d57f4644a81720282ee5bb01
rodrigo.xavier <rodrigo.xavier@host.docker.internal>
2020-09-28 16:18:48 -0300

    Alterações feitas:na Model para adequeção em API e no Repository

--- leitorCSV/src/main/java/br/com/rodrigo/estudo/leitorCSV/model/SMS.java
+++ leitorCSV/src/main/java/br/com/rodrigo/estudo/leitorCSV/model/SMS.java
@@ -7,28 +7,47 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "db_EnvioSMS")
 public class SMS {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	// Pegar o nome do cliente
 	@Column(length = 10)
+	@Autowired
 	private String nomeCliente;
 
 	// Tecnologia principal que o funcionario exerce
 	@Column(length = 13)
+	@Autowired
 	private String telCliente;
 
-	// Mensagem que será enviada - Com o numero maximo de caracteres permitido em um SMS
+	// Mensagem que será enviada - Com o numero maximo de caracteres permitido em
+	// um SMS
 	@Column(length = 160)
+	@Autowired
 	private String Mensagem;
 
-	
+	private Integer idSms;
+
+	private Integer idCliente;
+
+	public void SMS() {
+
+	}
+
+	public void sms(String nomeCliente, String telCliente, String Mensagem) {
+		this.nomeCliente = nomeCliente;
+		this.telCliente = telCliente;
+		this.Mensagem = Mensagem;
+	}
+
 }
--- leitorCSV/src/main/java/br/com/rodrigo/estudo/leitorCSV/repository/smsDTO.java
+++ leitorCSV/src/main/java/br/com/rodrigo/estudo/leitorCSV/repository/smsDTO.java
@@ -1,11 +1,12 @@
 package br.com.rodrigo.estudo.leitorCSV.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.rodrigo.estudo.leitorCSV.model.SMS;
 
 @Repository
-public interface smsDTO extends CrudRepository<SMS, Integer>{
+public interface smsDTO extends JpaRepository<SMS, Integer>{
 
 }
